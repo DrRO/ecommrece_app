7ef29bcd85c42901f0fa1f0e04ae6643ec47abd1
DrRO <omyahyayahya@yahoo.com>
2023-05-24 20:00:22 +0300

    Profile Screen UI and Logout

--- lib/registration_api_connection/users/fragments/profile_fragment.dart
+++ lib/registration_api_connection/users/fragments/profile_fragment.dart
@@ -1,12 +1,140 @@
+import 'package:ecommrece_app/registration_api_connection/users/user_preferences/user_pref.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import '../authentication/login_screen.dart';
+import '../user_preferences/current_user.dart';
 
 class ProfileFragmentScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Profile Fragment Screen"),
+  Widget userInfoItemProfile(IconData iconData, String userData) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.grey,
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 8,
+      ),
+      child: Row(
+        children: [
+          Icon(
+            iconData,
+            size: 30,
+            color: Colors.black,
+          ),
+          const SizedBox(
+            width: 16,
+          ),
+          Text(
+            userData,
+            style: const TextStyle(
+              fontSize: 15,
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.all(32),
+      children: [
+        Center(
+          child: Image.asset(
+            "images/man.png",
+            width: 240,
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        userInfoItemProfile(Icons.person, _currentUser.user.user_name),
+        const SizedBox(
+          height: 20,
+        ),
+        userInfoItemProfile(Icons.email, _currentUser.user.user_email),
+        const SizedBox(
+          height: 20,
+        ),
+        Center(
+          child: Material(
+            color: Colors.redAccent,
+            borderRadius: BorderRadius.circular(8),
+            child: InkWell(
+              onTap: () {
+                signOutUser();
+              },
+              borderRadius: BorderRadius.circular(32),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 30,
+                  vertical: 12,
+                ),
+                child: Text(
+                  "Sign Out",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  final CurrentUser _currentUser = Get.put(CurrentUser());
+
+  signOutUser() async {
+    var resultResponse = await Get.dialog(
+      AlertDialog(
+        backgroundColor: Colors.grey,
+        title: const Text(
+          "Logout",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        content: const Text(
+          "Are you sure?\nyou want to logout from app?",
+        ),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Get.back();
+              },
+              child: const Text(
+                "No",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              )),
+          TextButton(
+              onPressed: () {
+                Get.back(result: "loggedOut");
+              },
+              child: const Text(
+                "Yes",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              )),
+        ],
+      ),
+    );
+
+    if (resultResponse == "loggedOut") {
+      //delete-remove the user data from phone local storage
+      SaveUserInfo.removeUserInfo().then((value) {
+        Get.off(LoginScreen());
+      });
+    }
   }
 }
--- lib/registration_api_connection/users/user_preferences/user_pref.dart
+++ lib/registration_api_connection/users/user_preferences/user_pref.dart
@@ -5,6 +5,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../model/user.dart';
 
 class SaveUserInfo {
+  //Save user Data from SharedPreferences
+
   static Future<void> saveUserData(User userInfo) async {
     SharedPreferences sharedPref = await SharedPreferences.getInstance();
 
@@ -12,14 +14,23 @@ class SaveUserInfo {
     await sharedPref.setString("rememberUser", userJsonData);
   }
 
+  //Read user Data from SharedPreferences
+
   static Future<User?> readUser() async {
     User? currentUserInfo;
     SharedPreferences sharedPref = await SharedPreferences.getInstance();
     String? userInfo = sharedPref.getString("rememberUser");
     if (userInfo != null) {
-      Map<String, dynamic>userDataMap = jsonDecode(userInfo);
+      Map<String, dynamic> userDataMap = jsonDecode(userInfo);
       currentUserInfo = User.fromJson(userDataMap);
     }
     return currentUserInfo;
+  }
+
+  //Remove user data from SharedPreferences
+
+  static Future<void> removeUserInfo() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    await preferences.remove("rememberUser");
   }
 }
